33d3960cae07892fd21a2ab4ccb9445221f15d12
zhaixinyu249 <189373931@qq.com>
2026-07-02 20:32:58 +0800

    2

--- operation/xinjiqi.sh
+++ operation/xinjiqi.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # 添加密钥，禁用密码登录
 
+info_echo() {
+    # 流程信息
+    echo -e "\t\e[46m=== $1 ===\e[0m"
+}
+
 touch /root/.ssh/authorized_keys 2>/dev/null
 # 公钥
 public='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
@@ -13,3 +18,4 @@ echo "${public}" >>/root/.ssh/authorized_keys
 chattr +i /etc/ssh/sshd_config /root/.ssh/authorized_keys 2>/dev/null
 systemctl restart ssh
 systemctl restart sshd 2>/dev/null
+info_echo "添加完成，可以使用密钥登录"
