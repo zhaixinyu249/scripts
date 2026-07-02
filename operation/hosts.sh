5bbd80e92104e6dfed1e005f27d22ef386a0808e
zhaixinyu249 <189373931@qq.com>
2026-07-02 20:26:36 +0800

    1

--- operation/hosts.sh
+++ operation/hosts.sh
@@ -2,6 +2,7 @@
 # 配置本地hosts解析
 # 使用方法：bash <(curl -sSfLk https://github.com/zhaixinyu249/scripts/raw/refs/heads/main/hosts.sh) www.example.com
 
-ip=$(nslookup $1 | awk -F ' ' '/Address: [0-9]/{print $NF}')
-echo "${ip} $1" >> /etc/hosts
+ip=$(getent ahosts $1 | awk 'NR==1{print $1}')
+echo "$ip $1" >> /etc/hosts
+echo "=========================="
 tail /etc/hosts
