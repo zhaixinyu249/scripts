20fe95a0ee8e0fc3983b953f965fcb20a8e9cbd8
zhaixinyu249 <120649425+zhaixinyu249@users.noreply.github.com>
2026-07-02 12:02:10 +0800

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 配置本地hosts解析
+# 使用方法：bash <(curl -sSfLk https://github.com/zhaixinyu249/scripts/raw/refs/heads/main/hosts.sh) www.example.com
 
 ip=$(nslookup $1 | awk -F ' ' '/Address: [0-9]/{print $NF}')
 echo "${ip} $1" >> /etc/hosts
