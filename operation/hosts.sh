82251dd694a5cadaeb77f9f6da86df14c79e9504
zhaixinyu249 <189373931@qq.com>
2026-07-14 05:20:34 +0000

    update operation/hosts.sh.
    Signed-off-by: zhaixinyu249 <189373931@qq.com>

--- operation/hosts.sh
+++ operation/hosts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 配置本地hosts解析
-# 使用方法：bash <(curl -sSfLk https://github.com/zhaixinyu249/scripts/raw/refs/heads/main/hosts.sh) www.example.com
+# 使用方法：
+# 1）bash <(curl -sSfLk https://github.com/zhaixinyu249/scripts/raw/refs/heads/main/hosts.sh) www.example.com
+# 2）bash <(curl -sSfLk https://gitee.com/zhaixinyu249/scripts/raw/main/operation/hosts.sh) www.example.com
 
 ip=$(getent ahosts $1 | awk 'NR==1{print $1}')
 echo "$ip $1" >> /etc/hosts
